5e8c80bdae83cadedf998b03e1d1341c7dead1eb
Vasya <br2876@bs.amalnet.k12.il>
2026-01-07 13:10:49 +0200

    writing the credints

--- app/src/main/java/com/example/alert/MyActivityName.java
+++ app/src/main/java/com/example/alert/MyActivityName.java
@@ -1,7 +1,12 @@
 package com.example.alert;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MyActivityName extends AppCompatActivity {
 
@@ -10,4 +15,21 @@ public class MyActivityName extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_activity_name);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.name_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        String temp = item.getTitle().toString();
+        if (temp.equals("back")) {
+            finish();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
 }
