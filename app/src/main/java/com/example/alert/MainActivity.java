030a635fce62d1f40d785d5cf739cd58497158bc
Vasya <br2876@bs.amalnet.k12.il>
2026-01-07 09:49:17 +0200

    merraige

--- app/src/main/java/com/example/alert/MainActivity.java
+++ app/src/main/java/com/example/alert/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.alert;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,10 +11,28 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button mes;
+    Button img;
+    Button war;
+    Button cho;
+    Button col;
+
+
+
+    public void merraige(){
+        mes = (Button) findViewById(R.id.mess);
+        img = (Button) findViewById(R.id.img);
+        war = (Button) findViewById(R.id.war);
+        cho = (Button) findViewById(R.id.cho);
+        col = (Button) findViewById(R.id.col);
+    }
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        merraige();
     }
 }
